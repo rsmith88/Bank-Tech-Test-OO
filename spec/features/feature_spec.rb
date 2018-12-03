5e98b2aaf3fbc8e472bbd1a72d88778ae39dfc65
Raylene <smith.raylene@gmail.com>
2018-12-03 17:14:35 +0000

    Updated feature tests to mock date

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,12 +4,13 @@ require 'transaction'
 
 describe "Feature" do
   it 'allows users to interact with account' do
+    date = Time.now.strftime('%d/%m/%Y')
     account = Account.new
     account.deposit(1000)
     account.deposit(2000)
     account.withdraw(500)
     expect { account.statement.print }.to output(
-      "Date       || credit || debit || balance \n03/12/2018 ||  1000  ||  0  ||  1000\n03/12/2018 ||  2000  ||  0  ||  3000\n03/12/2018 ||  0  ||  500  ||  2500\n"
+      "Date       || credit || debit || balance \n#{date} ||  1000  ||  0  ||  1000\n#{date} ||  2000  ||  0  ||  3000\n#{date} ||  0  ||  500  ||  2500\n"
     )
       .to_stdout
     expect(account.statement.print).to eq("Thank you for choosing Tech-Test Bank!")
