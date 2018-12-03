64171b866bf1b24b455e962bf52fef70e6573bda
Raylene <smith.raylene@gmail.com>
2018-12-03 15:02:47 +0000

    Added doubles for statement spec

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,14 +9,25 @@ describe Statement do
   end
 
   describe '#print' do
-    it 'should print transactions with date, credit, debit, & total balance' do
+    it 'prints transactions with date, credit, debit, & balance' do
       account = Account.new
       account.deposit(1000)
+      statement = Statement.new(account.transactions)
+      expect { statement.print }.to output(
+        "Date       || credit || debit || balance \n#{Time.now.strftime('%d/%m/%Y')} ||  1000  ||  0  ||  1000\n"
+      )
+        .to_stdout
+    end
+
+    it 'prints transactions with date, credit, debit, & balance (stubbed)' do
+      transaction = double(:transaction, credit: 1000, debit: 0, balance: 1000, date: '03/12/2018')
+      account = double(:account, transactions: [transaction])
       statement = Statement.new(account.transactions)
       expect { statement.print }.to output(
         "Date       || credit || debit || balance \n03/12/2018 ||  1000  ||  0  ||  1000\n"
       )
         .to_stdout
     end
+
   end
 end
